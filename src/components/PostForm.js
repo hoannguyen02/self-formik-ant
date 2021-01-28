6fe3b499c49b583b5d4dadd7e1967de63419d121
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2021-01-29 06:35:11 +0700

    Apply basic validation with yup

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+let schema = yup.object().shape({
+  title: yup.string().required("This is required field"),
+});
 
 const PostForm = ({ initialValues, onSubmit }) => (
   <Formik
     initialValues={initialValues}
+    validationSchema={schema}
     onSubmit={(values) => {
       onSubmit(values);
     }}
@@ -11,6 +17,7 @@ const PostForm = ({ initialValues, onSubmit }) => (
     {({ isSubmitting }) => (
       <Form>
         <Field type='input' name='title' />
+        <ErrorMessage name='title' component='div' />
         <button type='submit' disabled={isSubmitting}>
           Submit
         </button>
